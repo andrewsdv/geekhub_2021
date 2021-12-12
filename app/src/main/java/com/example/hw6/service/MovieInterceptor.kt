1ba236978ed4ecae1bc549d148a45038bd74274e
Bogdan Boiko <bogdanboiko19996@gmail.com>
2021-12-12 23:38:58 +0300

    Fix color injection, move api key and base url to gradle

--- app/src/main/java/com/example/hw6/service/MovieInterceptor.kt
+++ app/src/main/java/com/example/hw6/service/MovieInterceptor.kt
@@ -1,14 +1,14 @@
 package com.example.hw6.service
 
+import com.example.hw6.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class MovieInterceptor : Interceptor {
-    private val apiKey = "4b5565bde59ab3b594114c4d7708b052"
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
         val originalUrl = request.url
-        val newUrl = originalUrl.newBuilder().addEncodedQueryParameter("api_key", apiKey).build()
+        val newUrl = originalUrl.newBuilder().addEncodedQueryParameter("api_key", BuildConfig.API_KEY).build()
         val newRequest = request.newBuilder().url(newUrl).build()
         return chain.proceed(newRequest)
     }
