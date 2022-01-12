01ec61bacfa048413d72f85825b579b63a3132e5
andrews <av.shevtsov@gmail.com>
2022-01-12 09:44:40 +0200

    Remove Toast.

--- app/src/main/java/com/example/hw6/MainActivity.kt
+++ app/src/main/java/com/example/hw6/MainActivity.kt
@@ -1,11 +1,6 @@
 package com.example.hw6
 
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import android.os.Build
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.hw6.adapter.PosterLoader
@@ -30,7 +25,6 @@ class MainActivity : AppCompatActivity(), PosterLoader {
         }
 
     override fun onPosterClicked(moviePreview: MoviePreview) {
-        if (isNetworkAvailable(this)) Toast.makeText(this, "No Internet connection", Toast.LENGTH_SHORT).show()
         replaceFragment(MovieDetailsFragment(moviePreview))
     }
 
@@ -41,32 +35,5 @@ class MainActivity : AppCompatActivity(), PosterLoader {
         } else {
             super.onBackPressed()
         }
-    }
-
-    private fun isNetworkAvailable(context: Context?): Boolean {
-        if (context == null) return false
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-            if (capabilities != null) {
-                when {
-                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
-                        return true
-                    }
-                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
-                        return true
-                    }
-                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
-                        return true
-                    }
-                }
-            }
-        } else {
-            val activeNetworkInfo = connectivityManager.activeNetworkInfo
-            if (activeNetworkInfo != null && activeNetworkInfo.isConnected) {
-                return true
-            }
-        }
-        return false
     }
 }
